fbb3beaec69c5773ff3f94bad16e1483607342a0
Portfolio-Premrutai <82368188+Portfolio-Premrutai@users.noreply.github.com>
2022-02-10 13:52:56 +0700

    02/10

--- vuetify_todo/src/views/Home.vue
+++ vuetify_todo/src/views/Home.vue
@@ -3,48 +3,32 @@
     <!-- <HelloWorld /> -->
     <!-- <h1>Home</h1> -->
     <v-container>
-      <v-row>
-        <v-col cols="10">
-          <v-text-field name="name" label="Task" id="id"></v-text-field>
-        </v-col>
-        <v-col cols="2">
-          <v-btn block color="success">add</v-btn>
-        </v-col>
-      </v-row>
-
-      <v-card>
-        <v-card-title primary-title>
-          <v-container>
-            <v-row
-              class="d-flex-row align-center"
-              v-for="(item, index) in todos"
-              :key="index"
-            >
-              <v-col cols="12" class="d-flex flex-row align-center">
-                <v-checkbox v-model="value" value="value"></v-checkbox>
-                <span>{{ item.title }}</span>
-                <v-spacer></v-spacer>
-                <v-btn color="error">x</v-btn>
-              </v-col>
-              <v-col cols="12">
-                <v-divider></v-divider>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-title>
-      </v-card>
+      <TodoAdd @onSubmit="addTask" />
+      <TodoList @onRemove="removeTask" :todos="todos" />
     </v-container>
   </div>
 </template>
 
 <script>
 // import HelloWorld from "../components/HelloWorld";
+import TodoList from "../components/TodoList.vue";
+import TodoAdd from "../components/TodoAdd.vue";
 
 export default {
   name: "home",
 
   components: {
     // HelloWorld,
+    TodoList,
+    TodoAdd,
+  },
+  methods: {
+    addTask(task) {
+      this.todos.push(task);
+    },
+    removeTask(id){
+      this.todos=this.todos.filter(item=>item.id !== id)
+    },
   },
   data() {
     return {
