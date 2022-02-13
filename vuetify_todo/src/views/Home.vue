bae0c8651e99bbe859a2c879a9f922868e12273b
Portfolio-Premrutai <82368188+Portfolio-Premrutai@users.noreply.github.com>
2022-02-13 11:16:52 +0700

    02/13

--- vuetify_todo/src/views/Home.vue
+++ vuetify_todo/src/views/Home.vue
@@ -5,6 +5,14 @@
     <v-container>
       <TodoAdd @onSubmit="addTask" />
       <TodoList @onRemove="removeTask" :todos="todos" />
+      <v-row class="d-flex flex-row justify-center align-center">
+        <img 
+        class="mt-10" 
+        contain
+        width="100"
+        height="100"
+        src="../assets/test.png" />
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -13,7 +21,7 @@
 // import HelloWorld from "../components/HelloWorld";
 import TodoList from "../components/TodoList.vue";
 import TodoAdd from "../components/TodoAdd.vue";
-
+import axios from "axios"
 export default {
   name: "home",
 
@@ -22,17 +30,22 @@ export default {
     TodoList,
     TodoAdd,
   },
+  async mounted() {
+   let result = await axios.get("https://jsonplaceholder.typicode.com/todos")
+  this.todos = result.data
+  },
   methods: {
     addTask(task) {
       this.todos.push(task);
     },
-    removeTask(id){
-      this.todos=this.todos.filter(item=>item.id !== id)
+    removeTask(id) {
+      this.todos = this.todos.filter((item) => item.id !== id);
     },
   },
   data() {
     return {
-      todos: [
+      todos:[],
+      todos_mockup: [
         { id: 1, title: "Task 1", completed: false },
         { id: 2, title: "Task 2", completed: true },
         { id: 3, title: "Task 3", completed: false },
