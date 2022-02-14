7b2f57313dfd00577aa8551ea069a1a13eea73f9
Portfolio-Premrutai <82368188+Portfolio-Premrutai@users.noreply.github.com>
2022-02-14 10:56:16 +0700

    02/14

--- vuetify_todo/src/views/Home.vue
+++ vuetify_todo/src/views/Home.vue
@@ -4,14 +4,16 @@
     <!-- <h1>Home</h1> -->
     <v-container>
       <TodoAdd @onSubmit="addTask" />
-      <TodoList @onRemove="removeTask" :todos="todos" />
+      <TodoList @onRemove="removeTask" :todos="todos | reversed" />
+      <!-- <TodoList @onRemove="removeTask" :todos="reversedTodo" /> -->
       <v-row class="d-flex flex-row justify-center align-center">
-        <img 
-        class="mt-10" 
-        contain
-        width="100"
-        height="100"
-        src="../assets/test.png" />
+        <img
+          class="mt-10"
+          contain
+          width="100"
+          height="100"
+          src="../assets/test.png"
+        />
       </v-row>
     </v-container>
   </div>
@@ -21,30 +23,56 @@
 // import HelloWorld from "../components/HelloWorld";
 import TodoList from "../components/TodoList.vue";
 import TodoAdd from "../components/TodoAdd.vue";
-import axios from "axios"
+import axios from "axios";
 export default {
   name: "home",
-
+  computed: {
+    reversedTodo() {
+      return this.todos.slice().reverse(); //#1
+    },
+  },
+  filters: {
+    reversed(value) {
+      return value.slice().reverse(); //#2
+    },
+  },
   components: {
     // HelloWorld,
     TodoList,
     TodoAdd,
   },
   async mounted() {
-   let result = await axios.get("https://jsonplaceholder.typicode.com/todos")
-  this.todos = result.data
+    let result = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    this.todos = result.data;
   },
   methods: {
-    addTask(task) {
-      this.todos.push(task);
+    async addTask(task) {
+      let result = await axios.post(
+        "https://jsonplaceholder.typicode.com/todos",
+        task
+      );
+      // alert(JSON.stringify(result.data))
+      this.todos.push(result.data);
     },
     removeTask(id) {
-      this.todos = this.todos.filter((item) => item.id !== id);
+      axios
+        .delete("https://jsonplaceholder.typicode.com/todos/" + id)
+        .then(result => {
+           alert(JSON.stringify(result.data));
+          // console.log(JSON.stringify(result.data))
+          this.todos = this.todos.filter(item => item.id !== id);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+        //  .catch(error => {
+        //     alert(error);
+        // });
     },
   },
   data() {
     return {
-      todos:[],
+      todos: [],
       todos_mockup: [
         { id: 1, title: "Task 1", completed: false },
         { id: 2, title: "Task 2", completed: true },
